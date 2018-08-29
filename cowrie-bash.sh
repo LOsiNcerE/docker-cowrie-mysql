7a66c973bb730902c822c6b91af28486ebee3cf7
LOsiNcerE <losincere@BJ2212.local>
2018-08-29 15:34:46 +0800

    cowrie to be finished

---
+++ cowrie-bash.sh
@@ -0,0 +1,1 @@
+docker exec -it docker-cowrie-mysql_cowrie_1 bash
---
+++ del.sh
@@ -0,0 +1,1 @@
+docker container rm -f docker-cowrie-mysql_cowrie_1 && docker image rm docker-cowrie-mysql_cowrie && docker container rm -f docker-cowrie-mysql_db_1 && docker image rm docker-cowrie-mysql_db
