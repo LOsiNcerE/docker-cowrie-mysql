afbff7b2296dbc506266592275c2b5c46e80515c
LOsiNcerE <losincere@BJ2212.local>
2018-09-05 16:48:17 +0800

    output engine mysql ready to run

--- del.sh
+++ del.sh
@@ -1,1 +1,1 @@
-docker container rm -f docker-cowrie-mysql_cowrie_1 && docker image rm docker-cowrie-mysql_cowrie && docker container rm -f docker-cowrie-mysql_db_1 && docker image rm docker-cowrie-mysql_db
+docker container prune && docker image prune && docker container rm -f docker-cowrie-mysql_cowrie_1 && docker image rm docker-cowrie-mysql_cowrie && docker container rm -f docker-cowrie-mysql_db_1 && docker image rm docker-cowrie-mysql_db
